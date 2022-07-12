d690c8be6ea09d782c03f91e3df96d59baf8f763
PJ <pje@telecommunity.com>
2022-07-12 07:20:53 -0400

    Switch build from rollup to ophidian+esbuild

---
+++ ophidian.config.mjs
@@ -0,0 +1,13 @@
+import Builder from "ophidian/build";
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
+const manifest = require("./manifest.json");
+
+new Builder("src/plugin.js")
+.withWatch(new URL('', import.meta.url).pathname)
+.assign({loader: {'.png': 'dataurl'}})
+.withSass()
+.withInstall(manifest.id)
+.build();
+
--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-import builder from "obsidian-rollup-presets";
-
-export default builder()
-.apply(c => c.output.sourcemap = "inline")
-.assign({input: "src/plugin.js"})
-.withInstall(__dirname)
-.build();
